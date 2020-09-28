12476fcc102407d46be1fb35ba7c03a39a616142
charlie9248it <trapuleng9248@gmail.com>
2020-09-28 16:26:55 +0200

    MVC PROJECT

---
+++ Mvc_Cricket/Controllers/MVCricketersController.cs
@@ -0,0 +1,94 @@
+﻿using Mvc_Cricket.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc_Cricket.Controllers
+{
+    public class MVCricketersController : Controller
+    {
+        CricketerDBContext1 db = new CricketerDBContext1();
+        // GET: MVCricketers
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: MVCricketers/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: MVCricketers/Create
+        public ActionResult Create()
+        {
+          
+            return View();
+        }
+
+        // POST: MVCricketers/Create
+        [HttpPost]
+        public ActionResult Create(Cricketer cricketer)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                
+                db.Cricketers.Add(cricketer);
+                db.SaveChanges();
+                return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: MVCricketers/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: MVCricketers/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: MVCricketers/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: MVCricketers/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Mvc_Cricket/Models/Cricketer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Mvc_Cricket.Models
+{
+    public class Cricketer
+    {
+
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int ODI { get; set; }
+        public int Test { get; set; }
+    }
+
+
+
+    public class CricketerDBContext1 :DbContext
+    {
+        public DbSet<Cricketer> Cricketers { get; set; }
+    }
+}
